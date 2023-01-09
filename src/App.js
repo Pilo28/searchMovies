ee2864b67c980785fb87071c0c4bc7527ebdaa08
pilo84 <pablidefelitto@gmail.com>
2023-01-09 15:41:50 +0100

    added connection with api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import './App.css';
+import { useEffect } from 'react';
+//f0973cd9
 
+const API_URL = "http://www.omdbapi.com?apikey=f0973cd9"
 
-function App() {
+const App = () => {
+
+  const searchMovies = async(title) =>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  }
+  useEffect(()=>{
+    searchMovies("Spiderman");
+  },[]);
+
   return (
     <div>
       <header>
-        <h1>App global</h1>
+        <h1>Movie Search</h1>
+        <nav>
+          <input placeholder='Search for Movies'/>
+        </nav>
       </header>
     </div>
   );
